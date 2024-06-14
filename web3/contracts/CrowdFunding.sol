090b9a1ceedfabd5d00b141c878393585a864758
Nikhilkps1904 <nikhilkps1904@gmail.com>
2024-06-14 20:37:22 +0530

    fiirst commit

---
+++ web3/contracts/CrowdFunding.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct  Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256 donations;
+    }
+    mapping (uint256 =>Campaign) public campaigns;
+    uint256 public numberofCampaigns = 0;
+
+    function createCampaign(address _owner,string memory _title,string memory _description,uint256  _target,
+    uint256 _deadline, string memory _image) public returns (uint256){
+        Campaign storage campaign = campaigns[numberofCampaigns];
+
+        require(campaign.deadline < block.timestamp, "the deadline should be date in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description =_description;
+        campaign.target = _traget;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberofCampaigns++;
+        return numberofCampaigns-1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaigns = campaigns[_id];
+
+        campaigns.donators.push(msg.sender);
+        campaigns.donations.push(amount);
+        (bool sent) = payable(campaigns.owner).call{value: amount}("");
+        if(sent){
+            campaigns.amountCollected = campaigns.amountCollected = amount;
+        }
+    }
+
+    function getDonators() {}
+
+    function getCampaign() {}
+}
